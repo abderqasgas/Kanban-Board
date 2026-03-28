85717e67d1239360623f78b39a6c0969c84a0164
Abdo <abderqasgas@gmail.com>
2026-03-27 22:42:08 -0500

    added js functions

---
+++ script.js
@@ -0,0 +1,33 @@
+const cards = document.querySelectorAll(".card");
+const lists = document.querySelectorAll(".list");
+
+
+for(const card of cards){
+    card.addEventListener("dragstart", dragStart)
+    card.addEventListener("dragend", dragEnd)
+}
+
+for (const list of lists){
+    list.addEventListener("dragover", dragOver)
+    list.addEventListener("dragenter", dragEnter)
+    list.addEventListener("dragleave", dragLeave)
+    list.addEventListener("drop", drop)
+}
+
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", this.id)
+}
+
+function dragEnd () {
+    console.log('drag ended')
+}
+
+function dragOver(){
+    e.preventDefault()
+}
+
+function dragEnter(e){
+    e.preventDefault();
+
+    this.classList.add("over")
+}
